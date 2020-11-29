cbb9061f743a0e6bc65874e25e89ed0a572fdd07
Hudson8811 <hudson88@yandex.ru>
2020-11-29 13:32:04 +0300

    add

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,89 +1,58 @@
 $(document).ready(function () {
 
+	var chartWrap = $('#numpurchases-chart-wrap'),
+		chart = $('#numpurchases-chart'),
+		topLayer = $('#numpurchases-chart-active-level'),
+		chartWrapH = chartWrap.height(),
+		chartWrapW = chartWrap.width();
 
-	$('.js-numpurchases-info-item').on('mouseenter',function(){
-		$(this).css('color', $(this).attr('data-hover-color'));
+	function resize_my_canvas() {
+		chartWrapH = chartWrap.height();
+		chartWrapW = chartWrap.width();
+		chart.attr('height', chartWrapH);
+		chart.attr('width', chartWrapW);
+		topLayer.attr('height', chartWrapH);
+		topLayer.attr('width', chartWrapW);
+
+	}
+	resize_my_canvas();
+	$(document).resize(function () {
+		resize_my_canvas();
 	});
-	$('.js-numpurchases-info-item').on('mouseleave',function(){
-		$(this).css('color', '');
-	});
 
-
-	$('.js-numpurchases-info-item').on('click',function(){
-		if(!$(this).hasClass('numpurchases-info-item--active')){
-			$(this).addClass('numpurchases-info-item--active').css('border-color', $(this).attr('data-hover-color'));
-
-			$(this).siblings().removeClass('numpurchases-info-item--active').css('border-color', '');
-		}
-	});
+	var chartItemSpace=3;
 
 
 	var ctx = document.getElementById('numpurchases-chart').getContext('2d');
 
 
 
-	gradient = ctx.createLinearGradient(50, 50, 0, 150);
-	gradient.addColorStop(0, 'blue');
-	gradient.addColorStop(0.5, 'green');
-	gradient.addColorStop(1, 'red');
 
-
-
-	backgroundColors = [gradient, '#4F5566', gradient, '#4F5566', '#4F5566'];
-
-
-
-
-	var myChart = new Chart(ctx, {
-		// The type of chart we want to create
-		type: 'doughnut',
-
-		// The data for our dataset
-		data: {
-			labels: ['41,27%', '11,73%', '7,28%', '6,57%', '6,06%'],
-			datasets: [{
-				label: 'My First dataset',
-				backgroundColor: backgroundColors,
-				borderColor: '#3B4255',
-				borderWidth: 2,
-				hoverBackgroundColor: '#5F6576',
-				data: [41.27, 11.73, 7.28, 6.57, 6.06],
-			}]
-		},
-
-		// Configuration options go here
-		options: {
-			cutoutPercentage: 30,
-			legend: {
-				display: false
-			},
-			tooltips: false,
-			events: ['mousemove', 'click'],
-			onHover: function (evt, activeElements) {
-
-			},
-			onClick: function (evt, elements) {
-				var datasetIndex;
-				var dataset;
-
-				if (elements.length) {
-					console.log(elements[0]);
-					var index = elements[0]._index;
-					datasetIndex = elements[0]._datasetIndex;
-
-					// Reset old state
-					dataset = myChart.data.datasets[datasetIndex];
-					dataset.backgroundColor = backgroundColors.slice();
-
-					//dataset.hoverBackgroundColor = '#4F5566';
-
-					dataset.backgroundColor[index] = 'red'; // click color
-					//dataset.hoverBackgroundColor[index] = 'red';
-				}
-
-				myChart.update();
-			}
+	function arraySum(array) {
+		var sum = 0;
+		for (var i = 0; i < array.length; i++) {
+			sum += array[i];
 		}
+		return (sum);
+	}
+
+	var backgroundColorsForGrad = [
+		['rgb(180,246,165)', 'rgb(98,219,70)'],
+		['rgb(255,170,86)', 'rgb(255,127,0)'],
+		['rgb(180,246,165)', 'rgb(98,219,70)'],
+		['rgb(255,170,86)', 'rgb(255,127,0)'],
+		['rgb(180,246,165)', 'rgb(98,219,70)']
+	]
+
+	var chartData = [41.27, 11.73, 7.28, 6.57, 6.06];
+	var chartDataInPercent = [];
+	var chartDataSum = arraySum(chartData);
+	var chartDataPercent = chartDataSum / 100;
+	var chartDataLength = chartData.length;
+
+
+	chartData.forEach(function (item, index, chartData) {
+		chartDataInPercent[index] = item / chartDataPercent;
 	});
 
 
@@ -93,14 +62,20 @@ $(document).ready(function () {
 
 
 
+	function getPoint(c1, c2, radius, angle) { //смещение точки по окружности
+		return [c1 + Math.cos(angle) * radius, c2 + Math.sin(angle) * radius];
+	}
+
+	function radToAngle(rad) {
+		return (rad * 180 / Math.PI);
+	}
+
+	function angleToRad(angle) {
+		return (angle * Math.PI / 180);
+	}
 
 
-
-
-	gradient = ctx.createLinearGradient(50, 50, 0, 150);
-	gradient.addColorStop(0, 'blue');
-	gradient.addColorStop(0.5, 'green');
-	gradient.addColorStop(1, 'red');
+/////////////////CUSTOM active chart
 
 
 	var canvas = document.getElementById("numpurchases-chart-active-level");
@@ -127,34 +102,195 @@ $(document).ready(function () {
 			canvas.stroke();
 			var numberOfParts = data.numberOfParts;
 			var parts = data.parts.pt;
-			var colors = data.colors.cs;
-			var df = 0;
-			for (var i = 0; i < numberOfParts; i++) {
-				if (i % 2 == 1) {
+			var df = -(Math.PI * 2) * (25 / 100);
 
+
+
+			var oneGradInPx = 2 * Math.PI * (chartWrapW / 2) / 360;
+			var skipRadians =angleToRad(chartItemSpace/oneGradInPx);
+			console.log(skipRadians);
+			for (var i = 0; i < numberOfParts; i++) { //numberOfParts
+				canvas.lineWidth = this.lineWidth;
+				canvas.beginPath();
+					canvas.arc(this.x, this.y, this.radius, df+skipRadians/2, df + (Math.PI * 2) * (parts[i] / 100)-skipRadians/2);
+
+				var startPoint = getPoint(this.x, this.y, this.radius + 0.5 * this.lineWidth, df);
+				var endPoint = getPoint(this.x, this.y, this.radius + 0.5 * this.lineWidth, df + (Math.PI * 2) * (parts[i] / 100));
+
+
+
+				var gradient = ctx.createLinearGradient(startPoint[0], startPoint[1], endPoint[0], endPoint[1]);
+				gradient.addColorStop(0, backgroundColorsForGrad[i][0]);
+				gradient.addColorStop(1, backgroundColorsForGrad[i][1]);
+				canvas.strokeStyle = gradient;
+				canvas.stroke();
+
+				canvas.closePath();
+
+				/*
+									canvas.beginPath();
+
+									canvas.lineWidth = 5;
+									canvas.strokeStyle = 'black';
+
+									console.log(radToAngle(0));
+									console.log(radToAngle(df));
+									var newPoint=getPoint(this.x, this.y, this.radius+0.5*this.lineWidth,df);
+									canvas.moveTo(newPoint[0], newPoint[1]); // Начало линии
+
+									var newPoint=getPoint(this.x, this.y, this.radius+0.5*this.lineWidth,df + (Math.PI * 2) * (parts[i] / 100));
+									canvas.lineTo(newPoint[0], newPoint[1]); // Конец линии
+
+									canvas.closePath();
+									canvas.stroke();*/
+
+				df += (Math.PI * 2) * (parts[i] / 100);
+			}
+		}
+
+
+		this.drawIndex = function (data, index = 0) {
+			canvas.lineWidth = this.lineWidth;
+			canvas.strokeStyle = this.strockStyle;
+
+			var numberOfParts = data.numberOfParts;
+			var parts = data.parts.pt;
+
+			var df = -(Math.PI * 2) * (25 / 100);
+
+			var oneGradInPx = 2 * Math.PI * (chartWrapW / 2) / 360;
+			var skipRadians =angleToRad(chartItemSpace/oneGradInPx);
+			console.log(skipRadians);
+
+			for (var i = 0; i < numberOfParts; i++) { //numberOfParts
+				if (i == index) {
+					canvas.lineWidth = this.lineWidth;
 					canvas.beginPath();
-					canvas.strokeStyle = colors[i];
-					canvas.arc(this.x, this.y, this.radius, df, df + (Math.PI * 2) * (parts[i] / 100));
+
+					canvas.shadowColor = backgroundColorsForGrad[i][0];
+					canvas.shadowBlur = 15;
+					canvas.arc(this.x, this.y, this.radius, df+skipRadians/2, df + (Math.PI * 2) * (parts[i] / 100)-skipRadians/2);
+
+					var startPoint = getPoint(this.x, this.y, this.radius + 0.5 * this.lineWidth, df);
+					var endPoint = getPoint(this.x, this.y, this.radius + 0.5 * this.lineWidth, df + (Math.PI * 2) * (parts[i] / 100));
 
 
 
+					var gradient = ctx.createLinearGradient(startPoint[0], startPoint[1], endPoint[0], endPoint[1]);
+					gradient.addColorStop(0, backgroundColorsForGrad[i][0]);
+					gradient.addColorStop(1, backgroundColorsForGrad[i][1]);
+					canvas.strokeStyle = gradient;
 					canvas.stroke();
+
+					canvas.closePath();
+
 				}
+
 				df += (Math.PI * 2) * (parts[i] / 100);
 			}
 		}
 	}
 	var data = {
-		numberOfParts: 4,
+		numberOfParts: chartDataInPercent.length,
 		parts: {
-			"pt": [20, 30, 25, 25]
-		}, //percentage of each parts
-		colors: {
-			"cs": ["red", "green", "blue", "yellow"]
-		} //color of each part
+			"pt": chartDataInPercent
+		}//percentage of each parts
 	};
 
 	var drawDount = new drawdountChart(chart);
-	drawDount.set(150, 150, 100, 0, Math.PI * 2, 30, "#fff");
-	drawDount.draw(data);
+	var chartStrokeW = 48 * chartWrapW / 100 / 2;//69.3
+	drawDount.set(chartWrapW / 2, chartWrapW / 2, chartWrapW / 2 - 15 - chartStrokeW / 2, 0, Math.PI * 2, chartStrokeW, "rgba(255,255,255,0)");
+	console.log(chartDataInPercent);
+	//drawDount.draw(data);
+	drawDount.drawIndex(data,0);
+
+
+
+
+
+
+	///////////////CHART.JS
+
+	var myChart = new Chart(ctx, {
+		// The type of chart we want to create
+		type: 'doughnut',
+
+		// The data for our dataset
+		data: {
+			labels: ['41,27%', '11,73%', '7,28%', '6,57%', '6,06%'],
+			datasets: [{
+				label: 'My First dataset',
+				backgroundColor: '#4F5566',
+				borderColor: '#3B4255',
+				borderWidth: chartItemSpace,
+				hoverBackgroundColor: '#5F6576',
+				data: chartDataInPercent,
+			}]
+		},
+
+		// Configuration options go here
+		options: {
+			cutoutPercentage: 30,
+			legend: {
+				display: false
+			},
+			tooltips: false,
+			events: ['mousemove', 'click'],
+			onHover: function (evt, activeElements) {
+
+			},
+			onClick: function (evt, elements) {
+				var datasetIndex;
+				var dataset;
+
+				if (elements.length) {
+					console.log(elements[0]);
+					/*
+					var index = elements[0]._index;
+					datasetIndex = elements[0]._datasetIndex;
+*/
+
+				}
+
+				myChart.update();
+			}
+		}
+	});
+
+
+	///////////////events
+
+
+	$('.js-numpurchases-info-item').on('mouseenter', function () {
+		$(this).css('color', $(this).attr('data-hover-color'));
+	});
+	$('.js-numpurchases-info-item').on('mouseleave', function () {
+		$(this).css('color', '');
+	});
+
+
+	$('.js-numpurchases-info-item').on('click', function () {
+		if (!$(this).hasClass('numpurchases-info-item--active')) {
+			$(this).addClass('numpurchases-info-item--active').css('border-color', $(this).attr('data-hover-color'));
+
+			$(this).siblings().removeClass('numpurchases-info-item--active').css('border-color', '');
+		}
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
