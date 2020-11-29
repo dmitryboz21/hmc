cb4169c9bcbf72fd35ae919a8761bb4e1a1f2cd1
Hudson8811 <hudson88@yandex.ru>
2020-11-29 15:49:49 +0300

    add

--- build/js/main.js
+++ build/js/main.js
@@ -14,29 +14,34 @@
 /* my scripts */
 
 
+var resize_my_canvas;
 $(document).ready(function () {
+
+	function roundEven(d) { //округление до четного
+		return Math.round(d / 2) * 2;
+	}
 
 	var chartWrap = $('#numpurchases-chart-wrap'),
 		chart = $('#numpurchases-chart'),
 		topLayer = $('#numpurchases-chart-active-level'),
-		chartWrapH = chartWrap.height(),
-		chartWrapW = chartWrap.width();
+		chartWrapH,
+		chartWrapW;
 
-	function resize_my_canvas() {
-		chartWrapH = chartWrap.height();
-		chartWrapW = chartWrap.width();
+	resize_my_canvas = function () {
+		chartWrapH = roundEven(chartWrap.height());
+		chartWrapW = roundEven(chartWrap.width());
 		chart.attr('height', chartWrapH);
 		chart.attr('width', chartWrapW);
 		topLayer.attr('height', chartWrapH);
 		topLayer.attr('width', chartWrapW);
 
 	}
-	resize_my_canvas();
+	resize_my_canvas();;
 	$(document).resize(function () {
 		resize_my_canvas();
 	});
 
-	var chartItemSpace=3;
+	var chartItemSpace = 3;
 
 
 	var ctx = document.getElementById('numpurchases-chart').getContext('2d');
@@ -91,144 +96,94 @@ $(document).ready(function () {
 	}
 
 
-/////////////////CUSTOM active chart
+	/////////////////CUSTOM active chart
 
 
-	var canvas = document.getElementById("numpurchases-chart-active-level");
-	var chart = canvas.getContext("2d");
+	var canvasTopLevel = document.getElementById("numpurchases-chart-active-level");
+	var chartTopLevel = canvasTopLevel.getContext("2d");
 
 	function drawdountChart(canvas) {
 
 		this.x, this.y, this.radius, this.lineWidth, this.strockStyle, this.from, this.to = null;
 		this.set = function (x, y, radius, from, to, lineWidth, strockStyle) {
-			this.x = x;
-			this.y = y;
-			this.radius = radius;
-			this.from = from;
-			this.to = to;
-			this.lineWidth = lineWidth;
-			this.strockStyle = strockStyle;
-		}
-
-		this.draw = function (data) {
-			canvas.beginPath();
-			canvas.lineWidth = this.lineWidth;
-			canvas.strokeStyle = this.strockStyle;
-			canvas.arc(this.x, this.y, this.radius, this.from, this.to);
-			canvas.stroke();
-			var numberOfParts = data.numberOfParts;
-			var parts = data.parts.pt;
-			var df = -(Math.PI * 2) * (25 / 100);
+				this.x = x;
+				this.y = y;
+				this.radius = radius;
+				this.from = from;
+				this.to = to;
+				this.lineWidth = lineWidth;
+				this.strockStyle = strockStyle;
+			},
 
 
 
-			var oneGradInPx = 2 * Math.PI * (chartWrapW / 2) / 360;
-			var skipRadians =angleToRad(chartItemSpace/oneGradInPx);
-			console.log(skipRadians);
-			for (var i = 0; i < numberOfParts; i++) { //numberOfParts
+			this.drawIndex = function (data, index = 0, start = 0, end = 100) {
 				canvas.lineWidth = this.lineWidth;
-				canvas.beginPath();
-					canvas.arc(this.x, this.y, this.radius, df+skipRadians/2, df + (Math.PI * 2) * (parts[i] / 100)-skipRadians/2);
+				canvas.strokeStyle = this.strockStyle;
 
-				var startPoint = getPoint(this.x, this.y, this.radius + 0.5 * this.lineWidth, df);
-				var endPoint = getPoint(this.x, this.y, this.radius + 0.5 * this.lineWidth, df + (Math.PI * 2) * (parts[i] / 100));
+				var numberOfParts = data.numberOfParts;
+				var parts = data.parts.pt;
 
+				var df = -(Math.PI * 2) * (25 / 100);
 
+				var oneGradInPx = 2 * Math.PI * (chartWrapW / 2) / 360;
+				var skipRadians = angleToRad(chartItemSpace / oneGradInPx);
+				console.log(skipRadians);
 
-				var gradient = ctx.createLinearGradient(startPoint[0], startPoint[1], endPoint[0], endPoint[1]);
-				gradient.addColorStop(0, backgroundColorsForGrad[i][0]);
-				gradient.addColorStop(1, backgroundColorsForGrad[i][1]);
-				canvas.strokeStyle = gradient;
-				canvas.stroke();
+				for (var i = 0; i < numberOfParts; i++) { //numberOfParts
+					if (i == index) {
+						canvas.lineWidth = this.lineWidth;
+						canvas.beginPath();
 
-				canvas.closePath();
+						canvas.shadowColor = backgroundColorsForGrad[i][0];
+						canvas.shadowBlur = 15;
+						canvas.arc(this.x, this.y, this.radius, df + skipRadians / 2, df + (Math.PI * 2) * (parts[i] / 100) - skipRadians / 2);
 
-				/*
-									canvas.beginPath();
-
-									canvas.lineWidth = 5;
-									canvas.strokeStyle = 'black';
-
-									console.log(radToAngle(0));
-									console.log(radToAngle(df));
-									var newPoint=getPoint(this.x, this.y, this.radius+0.5*this.lineWidth,df);
-									canvas.moveTo(newPoint[0], newPoint[1]); // Начало линии
-
-									var newPoint=getPoint(this.x, this.y, this.radius+0.5*this.lineWidth,df + (Math.PI * 2) * (parts[i] / 100));
-									canvas.lineTo(newPoint[0], newPoint[1]); // Конец линии
-
-									canvas.closePath();
-									canvas.stroke();*/
-
-				df += (Math.PI * 2) * (parts[i] / 100);
-			}
-		}
-
-
-		this.drawIndex = function (data, index = 0) {
-			canvas.lineWidth = this.lineWidth;
-			canvas.strokeStyle = this.strockStyle;
-
-			var numberOfParts = data.numberOfParts;
-			var parts = data.parts.pt;
-
-			var df = -(Math.PI * 2) * (25 / 100);
-
-			var oneGradInPx = 2 * Math.PI * (chartWrapW / 2) / 360;
-			var skipRadians =angleToRad(chartItemSpace/oneGradInPx);
-			console.log(skipRadians);
-
-			for (var i = 0; i < numberOfParts; i++) { //numberOfParts
-				if (i == index) {
-					canvas.lineWidth = this.lineWidth;
-					canvas.beginPath();
-
-					canvas.shadowColor = backgroundColorsForGrad[i][0];
-					canvas.shadowBlur = 15;
-					canvas.arc(this.x, this.y, this.radius, df+skipRadians/2, df + (Math.PI * 2) * (parts[i] / 100)-skipRadians/2);
-
-					var startPoint = getPoint(this.x, this.y, this.radius + 0.5 * this.lineWidth, df);
-					var endPoint = getPoint(this.x, this.y, this.radius + 0.5 * this.lineWidth, df + (Math.PI * 2) * (parts[i] / 100));
+						var startPoint = getPoint(this.x, this.y, this.radius + 0.5 * this.lineWidth, df);
+						var endPoint = getPoint(this.x, this.y, this.radius + 0.5 * this.lineWidth, df + (Math.PI * 2) * (parts[i] / 100));
 
 
 
-					var gradient = ctx.createLinearGradient(startPoint[0], startPoint[1], endPoint[0], endPoint[1]);
-					gradient.addColorStop(0, backgroundColorsForGrad[i][0]);
-					gradient.addColorStop(1, backgroundColorsForGrad[i][1]);
-					canvas.strokeStyle = gradient;
-					canvas.stroke();
+						var gradient = ctx.createLinearGradient(startPoint[0], startPoint[1], endPoint[0], endPoint[1]);
+						gradient.addColorStop(0, backgroundColorsForGrad[i][0]);
+						gradient.addColorStop(1, backgroundColorsForGrad[i][1]);
+						canvas.strokeStyle = gradient;
+						canvas.stroke();
 
-					canvas.closePath();
+						canvas.closePath();
 
+					}
+
+					df += (Math.PI * 2) * (parts[i] / 100);
 				}
+			},
 
-				df += (Math.PI * 2) * (parts[i] / 100);
+			this.clear = function () {
+				console.log(this.radius*2, this.radius*2);
+				canvas.clearRect(0, 0, canvas.canvas.height+30, canvas.canvas.height+30);
 			}
-		}
 	}
 	var data = {
 		numberOfParts: chartDataInPercent.length,
 		parts: {
 			"pt": chartDataInPercent
-		}//percentage of each parts
+		} //percentage of each parts
 	};
 
-	var drawDount = new drawdountChart(chart);
-	var chartStrokeW = 66 * chartWrapW / 100 / 2;//66
+	var drawDount = new drawdountChart(chartTopLevel);
+	var chartStrokeW = 33.7 * chartWrapW / 100 / 2; //66
 
 	console.log(chartStrokeW);
 	drawDount.set(chartWrapW / 2, chartWrapW / 2, chartWrapW / 2 - 15 - chartStrokeW / 2, 0, Math.PI * 2, chartStrokeW, "rgba(255,255,255,0)");
 	console.log(chartDataInPercent);
 	//drawDount.draw(data);
-	drawDount.drawIndex(data,0);
-
-
+	drawDount.drawIndexA(data, 0);
 
 
 
 
 	///////////////CHART.JS
-
+/*
 	var myChart = new Chart(ctx, {
 		// The type of chart we want to create
 		type: 'doughnut',
@@ -240,6 +195,7 @@ $(document).ready(function () {
 				label: 'My First dataset',
 				backgroundColor: '#4F5566',
 				borderColor: '#3B4255',
+				hoverBorderColor :  '#3B4255',
 				borderWidth: chartItemSpace,
 				hoverBackgroundColor: '#5F6576',
 				data: chartDataInPercent,
@@ -248,7 +204,8 @@ $(document).ready(function () {
 
 		// Configuration options go here
 		options: {
-			cutoutPercentage: 30,
+			responsive: false,
+			cutoutPercentage: 63.7,
 			legend: {
 				display: false
 			},
@@ -263,10 +220,10 @@ $(document).ready(function () {
 
 				if (elements.length) {
 					console.log(elements[0]);
-					/*
-					var index = elements[0]._index;
-					datasetIndex = elements[0]._datasetIndex;
-*/
+
+					//var index = elements[0]._index;
+					//datasetIndex = elements[0]._datasetIndex;
+
 
 				}
 
@@ -274,7 +231,7 @@ $(document).ready(function () {
 			}
 		}
 	});
-
+*/
 
 	///////////////events
 
